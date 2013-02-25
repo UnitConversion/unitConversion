3d0f06f42d355dc3be760cab3ff3dac4bf019283
Guobao Shen <devnull@localhost>
2013-02-25 13:18:07 -0500

    Add database with data

--- example/nsls2/rdbinfo.py
+++ example/nsls2/rdbinfo.py
@@ -4,7 +4,13 @@ Created on Feb 5, 2013
 @author: shengb
 '''
 
-host = ''
-user = ''
-pw = ''
-db = ''
+#host="localhost"
+host="/opt/local/var/run/mysql55/mysqld.sock"
+user = 'shengb'
+pw = 'nsls2_mysql'
+db='municonv'
+
+unix_socket="/opt/local/var/run/mysql55/mysqld.sock"
+
+#conn = mysql.connect(host=host,user=user,passwd=passwd, db=db)
+#conn = mysql.connect(unix_socket="/opt/local/var/run/mysql55/mysqld.sock",user=user,passwd=passwd, db=db)
