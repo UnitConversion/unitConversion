bd7e14c8a1dfb6b79f686759764529ae7ecf108f
Guobao Shen <devnull@localhost>
2013-02-28 16:33:56 -0500

    initial lattice data api

---
+++ physics_dev/pylattice/__init__.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 28, 2013
+
+@author: shengb
+'''
+
+__version__ = '1.0.0'
+__all__ = ['version']
---
+++ physics_dev/pylattice/lattice.py
@@ -0,0 +1,23 @@
+'''
+This module is to interface with lattice table.
+
+Created on Feb 28, 2013
+
+@author: shengb
+'''
+
+class lattice(object):
+    def __init__(self, conn):
+        ''''''
+        self.conn = conn
+
+    def retrievelattice(self, name, description=None, ):
+        ''''''
+#        sql = '''
+#        select lattice_name, lattice_description, create_by, create_date, update_by, update_date
+#        '''
+        
+        
+
+
+
