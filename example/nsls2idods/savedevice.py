107ce8d256e6c3e28420e5666987865a29965310
dejande <159643@gmail.com>
2014-10-20 15:37:18 +0200

    A clearer example of savedevice.py made.
    Methods in BNL\clientapi\idodspy\idodsclient.py
    added:
    - saveDevice
    - retrieveDevice
    - updateDevice
    
    A clear example made in unitConversion\example\nsls2idods\savedevice.py,
    how to save a device.

--- example/nsls2idods/savedevice.py
+++ example/nsls2idods/savedevice.py
@@ -25,20 +25,26 @@ except ImportError:
 
 __url = 'http://localhost:8000/id/device/'
 __jsonheader = {'content-type': 'application/json', 'accept': 'application/json'}
+
+# Initialize connection with credentials in etc/idodsclient.conf
 client = IDODSClient(BaseURL=__url)
 
-# Save component type if it does not exist
+# Variable initialization
+cmpnt_type_name = 'New Test Type 5'
+cmpnt_type_description = 'Component type description'
+device_name = 'NTT4-2'
+device_name_new = 'NTT4XX-2'
+device_description = 'Device description.'
+device_location = 'Device location.'
 
-if len(client.retrieveComponentType('device type')) == 0:
-    client.saveComponentType('device type')
+# Save a new device, here are all possible parameters. Necesary parameters are device_name and cmpnt_type_name. Returns an id of a newly saved device.
+deviceID = client.saveDevice(device_name=device_name, cmpnt_type_name=cmpnt_type_name, device_description=device_description, device_coordinatecenter=None, cmpnt_type_description=cmpnt_type_description, cmpnt_type_props=None)
 
-# Save device
-if len(client.retrieveInstall('name')) == 0:
-    client.saveInstall('name', cmpnt_type_name='device type')
+# Retrieve a device using all parameters.
+client.retrieveDevice(device_name, description=None, cmpnt_type_name=None, coordinatecenter=None)
 
-# Update install
-if len(client.retrieveInstall('new name')) == 0:
-    client.updateInstall('name', 'new name')
+# Update a device using all parameters.
+client.updateDevice(device_name, device_name_new, description=None, cmpnt_type_name=None, coordinatecenter=None)
 
 # Save online data
 savedOnlineData = client.saveOnlineData('new name', status=1, meas_time='2014-09-16')
@@ -46,14 +52,12 @@ savedOnlineData = client.saveOnlineData('new name', status=1, meas_time='2014-09
 # Update online data
 client.updateOnlineData(savedOnlineData['id'], rawdata_path='/usr/sth')
 
-
 # Update online data with feedforward binary data
 with open('Desert.jpg', 'rb') as f:
     client.updateOnlineData(savedOnlineData['id'], feedforward_file_name='desert image', feedforward_data=f)
 
 # Retrieve online data by status
 data = client.retrieveOnlineData(status=1)
-print data.keys()
 
 # Write retrieved image to a file
 if len(data.keys()) > 0 and data[data.keys()[0]]['feedforward_data'] is not None:
@@ -63,8 +67,6 @@ if len(data.keys()) > 0 and data[data.keys()[0]]['feedforward_data'] is not None
 
 # Retrieve online data by rawdata path
 data = client.retrieveOnlineData(rawdata_path='/usr/sth')
-print data.keys()
 
 # Retrieve online data by rawdata path
 data = client.retrieveOnlineData(meas_time=json.dumps(['2014-09-01', '2014-09-15']))
-print data.keys()
